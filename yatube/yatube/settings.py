840135d021a5a1596be63e9ecb0cc09654a5571d
Alexey Kuzenkov <kuzenkovag@yandex.ru>
2023-03-31 18:05:50 +0600

    Update name secret_key

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
